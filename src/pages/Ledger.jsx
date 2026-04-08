f0f59fa0c946ce3ea92a9bb8f0aca7a697547466
Arshad Aman <38555984+ArshadAman@users.noreply.github.com>
2026-04-09 01:06:48 +0530

    feat: add Link import for navigation in Ledger component

--- src/pages/Ledger.jsx
+++ src/pages/Ledger.jsx
@@ -14,6 +14,7 @@ import Layout from '../components/Layout';
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { PlusIcon, BanknotesIcon, XMarkIcon, DocumentArrowUpIcon, UsersIcon, MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { Link } from 'react-router-dom';
 
 const Ledger = () => {
   const [selectedCustomer, setSelectedCustomer] = useState('');
